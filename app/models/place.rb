a2f97f5d439bdc416859ebbc286d43ceacb95f6f
Ryan Chang <ry4nch4ng@gmail.com>
2016-07-24 16:52:14 -0700

    finish collection_spec.rb passed tests

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place
   include Mongoid::Document
+  attr_accessor :id, :formatted_address, :location, :address_components
 
   def self.mongo_client
     Mongoid::Clients.default
@@ -12,5 +13,16 @@ class Place
   def self.load_all(f)
     array = JSON.parse(f.read)
     collection.insert_many(array)
+  end
+
+  def initialize(params={})
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:geometry][:location || :geolocation])
+
+    @address_components = []
+    params[:address_components].each do |address_component|
+      @address_components << AddressComponent.new(address_component)
+    end
   end
 end
